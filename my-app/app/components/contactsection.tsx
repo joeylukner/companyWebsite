a469c59a3f24c55b988a2ce5a0a2293e0c97fd54
Joseph <jlukner@tspartnersinc.com>
2024-08-06 09:29:04 -0400

    property card translate adjusted, our interests building resized

--- my-app/app/components/contactsection.tsx
+++ my-app/app/components/contactsection.tsx
@@ -8,7 +8,7 @@ export default function ContactSection() {
       <div className="vertical-line">
       </div>
       <div className="contact-info">
-        <h5>Contact:<br />jangelella@tspartnersinc.com<br /> 610.768.1105 <br />Contact Us!</h5>
+        <h5>Contact:<br />jangelella@tspartnersinc.com<br /> 610-768-1105 <br />Contact Us!</h5>
       </div>
     </section>
   );
